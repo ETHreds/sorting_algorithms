87408da683201b6161c65a203848d2476ed2b4e5
Joe Jnr <jcmaglaw@gmail.com>
2022-10-11 12:59:29 +0300

    Betty compliance

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,23 @@
 #include "sort.h"
 
 /**
- *bubble_sort- sort stuff
- **array- pointer to array
- *size: sie of the array to be sort
+ *bubble_sort- Bubble sort in ascending order
+ *@array: array to be sorted
+ *@size: sie of the array to be sort
  *
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i,j;
+	size_t i, j;
 	int temp;
 
 	/** Itirate over whole array **/
 	for (i = 0; i < size; i++)
 	{
 		/** Itirate the swapping **/
-		for (j = 0; j < (size - i -1); j++)
+		for (j = 0; j < (size - i - 1); j++)
 		{
-			
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
